745f9ea13f885157b77fb34071f8b8bcda24410e
SaravanarajuR <SaravanarajuR>
2023-07-20 01:29:06 +0530

    added signup and register

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "/client/build/")));
-
-app.get("/", (req, res) => {
-  res.sendFile("index.html");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-app.listen("https://compannion.azurewebsites.net/", () => {
+app.listen(3000, () => {
   console.log("server running");
 });
